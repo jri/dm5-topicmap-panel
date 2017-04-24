942e6943ee2a6d3e97b912bc3a78f07afc4ffe1b
Jörg Richter <jri@deepamehta.de>
2017-04-24 15:39:39 +0200

    Initial commit

---
+++ src/topicmap-panel.js
@@ -0,0 +1,23 @@
+const state = {
+  topicmap: undefined,
+  renderer: require('./components/CytoscapeRenderer.vue')
+}
+
+const actions = {
+  registerTopicmapRenderer (_, renderer) {
+    if (state.renderer) {
+      // ### TODO: register multiple renderers by URI
+      throw new Error('Only one topicmap renderer can be registered')
+    }
+    state.renderer = renderer
+  },
+  setTopicmapData (_, topicmap) {
+    console.log('setTopicmapData', topicmap)
+    state.topicmap = topicmap
+  }
+}
+
+export default {
+  state,
+  actions
+}
